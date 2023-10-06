e705e9d383392d73934d03a39c4cd999b1e5c52c
Shrey <shreyc45@gmail.com>
2023-10-06 11:11:49 -0600

    sum of odd solution

---
+++ sum-of-odd.js
@@ -0,0 +1,13 @@
+//Sum of odd numbers
+
+           //    1
+         //   3     5
+       // 7     9    11
+   // 13    15    17    19
+//21    23    25    27    29
+
+function rowSumOddNumbers(n) {
+	// TODO
+  
+  return n*n*n
+}
