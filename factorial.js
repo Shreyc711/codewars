669b54e6c4f7caef22d00ee5e7fe437b8d30bd5a
Shrey <shreyc45@gmail.com>
2023-12-11 09:55:34 -0600

    factorial solution

---
+++ factorial.js
@@ -0,0 +1,12 @@
+//factorial function
+
+function factorial(n){
+    //your code here
+   
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
