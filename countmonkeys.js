fc54dc8636487160db36192861ed5a477808d5d4
Shrey <shreyc45@gmail.com>
2024-01-08 08:34:23 -0600

    count monkeys solution

---
+++ countmonkeys.js
@@ -0,0 +1,9 @@
+//help your son count monkeys
+function monkeyCount(n) {
+    // your code here
+      var result = [];
+      for(let i = 1;i <= n;i++){
+       result.push(i)
+      }
+      return result
+    }
