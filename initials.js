c7efb6d0cfaf6a0e8c05a3d5835e4eddec37d6da
Shrey <shreyc45@gmail.com>
2024-01-24 08:40:24 -0600

    codewars solution

---
+++ initials.js
@@ -0,0 +1,14 @@
+//return the initials
+
+function abbrevName(name){
+
+    // code away
+  
+  let spl = name.split(" ");
+let upper = spl[0][0] +"."+spl[1][0]
+let fin = upper.toUpperCase();
+  return fin
+  
+ 
+
+}
