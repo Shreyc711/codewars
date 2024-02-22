e7513f5bde5e018bc9ca0847153fbd551942671a
Shrey <shreyc45@gmail.com>
2024-02-22 10:59:38 -0600

    codewars solution

---
+++ findFactor.js
@@ -0,0 +1,11 @@
+//if factor return true else return false
+
+function checkForFactor (base, factor) {
+    // code here
+    if(base % factor == 0){
+      return true;
+    }
+    else{
+      return false
+    }
+    }
