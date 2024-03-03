f8b103843025d24b647fca2775228cf9baa50a37
Shrey <shreyc45@gmail.com>
2024-03-03 09:35:58 -0600

    split arr solution

---
+++ splitArr.js
@@ -0,0 +1,26 @@
+//Scenario
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+// Notes
+// Array size is at least 1.
+// All numbers will be positive.
+
+function rowWeights(array){
+    //your code here
+  let t1 = 0;
+    let t2 = 0;
+    
+   let arr = array.forEach(function(elem,i){
+     if(i % 2 == 0){
+       t1 += elem
+     }else{
+       t2 += elem
+     }
+   })
+  var a1 = [t1,t2]
+    return a1
+    }
