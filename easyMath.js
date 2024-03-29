fd5c471459fe15a7843c23249db2a87a3acae8db
Shrey <shreyc45@gmail.com>
2024-03-29 09:06:12 -0600

    easy math solution

---
+++ easyMath.js
@@ -0,0 +1,17 @@
+function basicOp(operation, value1, value2){
+    //Code
+  switch(operation){
+    case "+":
+      return value1 + value2;
+      break;
+       case "-":
+      return value1 - value2;
+      break;
+       case "*":
+      return value1 * value2;
+      break;
+       case "/":
+      return value1 / value2;
+      break;
+    }
+  }
