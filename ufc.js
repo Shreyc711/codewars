e1bdd23802a23dfecca74051afbb5e21ab4a73c9
Shreyc711 <shreyc45@gmail.com>
2024-06-26 12:00:52 -0400

    ufc solution

---
+++ ufc.js
@@ -0,0 +1,8 @@
+var quote = function (fighter) {
+    // your code here
+    switch (fighter.toLowerCase()) {
+        case 'conor mcgregor': return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+        case 'george saint pierre': return "I am not impressed by your performance.";
+        default: 'Who are you?'
+    }
+};
