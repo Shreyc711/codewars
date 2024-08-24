7b91740ecae57dc95116378eaf4786f3f139de67
Shreyc711 <shreyc45@gmail.com>
2024-08-24 15:41:26 -0400

    color the ghost solution

---
+++ colorTheGhost.js
@@ -0,0 +1,5 @@
+var Ghost = function() {
+    var colors = ["white", "yellow", "purple", "red"];
+    var colorIndex = Math.floor(Math.random() * (colors.length));
+    this.color = colors[colorIndex];
+  };
