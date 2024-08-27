29ebff5f7078d1e884431e54a33a87a21c6087f2
Shreyc711 <shreyc45@gmail.com>
2024-08-27 16:25:56 -0400

    is balanced or not solution

---
+++ isBalanced.js
@@ -0,0 +1,11 @@
+function balancedNum(number) {
+
+    let str = `${number}`
+    ,   len = (str.length - (str.length % 2 ? -1 : -2)) / 2
+    ,   sum = digits => [ ...digits ].reduce((a, b) => a + +b, 0);
+  
+    return sum(str.slice(0, len)) === sum(str.slice(-len)) 
+    ? 'Balanced' 
+    : 'Not Balanced';
+  
+  }
